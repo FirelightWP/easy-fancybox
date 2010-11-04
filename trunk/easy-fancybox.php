25d10858892eebc07ae6c54a18bf68369293d474
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-04 23:58:10 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/easy-fancybox@307945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/easy-fancybox.php
+++ trunk/easy-fancybox.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy FancyBox
 Plugin URI: http://4visions.nl/en/wordpress-plugins/easy-fancybox/
 Description: Hassle-free, no-settings, auto-enable <a href="http://fancybox.net/">FancyBox 1.3.3</a> on all image links including BMP, GIF, JPG, JPEG, and PNG. Uses packed Javascript. Happy with it? Please leave me a small <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Easy%20FancyBox&amp;item_number=1%2e3%2e3&no_shipping=0&tax=0&bn=PP%2dDonationsBF&charset=UTF%2d8&lc=us">TIP</a> for development and support on this plugin and please consider a DONATION to the <a href="http://fancybox.net/">FancyBox project</a>.
-Version: 1.3.3.4
+Version: 1.3.3-4
 Author: RavanH
 Author URI: http://4visions.nl/
 */
