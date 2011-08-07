1a0f36184ce08028605b7978e682fa99051d3bfe
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-07 00:26:54 +0000

    youtube url pregmatch bug
    git-svn-id: https://plugins.svn.wordpress.org/easy-fancybox/tags/1.3.4.9@420203 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easy-fancybox-settings.php
+++ easy-fancybox-settings.php
@@ -791,7 +791,7 @@ function easy_fancybox_settings(){
 					), */
 				'onStart' => array ( 
 						'noquotes' => true,
-						'default' => 'function(selectedArray, selectedIndex, selectedOpts) { selectedOpts.href = selectedArray[selectedIndex].href.replace(new RegExp(\'youtu.be\', \'i\'), \'www.youtube.com/embed\').replace(new RegExp(\'watch\\\?v=([a-z0-9]+)(&|\\\?)?(.*)\', \'i\'), \'embed/$1?version=3&$3\') }'
+						'default' => 'function(selectedArray, selectedIndex, selectedOpts) { selectedOpts.href = selectedArray[selectedIndex].href.replace(new RegExp(\'youtu.be\', \'i\'), \'www.youtube.com/embed\').replace(new RegExp(\'watch\\\?v=([a-z0-9\_\-]+)(&|\\\?)?(.*)\', \'i\'), \'embed/$1?version=3&$3\') }'
 					)
 				)
 			),
