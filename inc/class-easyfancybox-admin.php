6f7a4b807e468e50ae9bb466f076635a307375d2
RavanH <ravanhagen@gmail.com>
2017-11-26 02:53:11 +0100

    admin page line breaks hidden in WP 4.9

--- inc/class-easyfancybox-admin.php
+++ inc/class-easyfancybox-admin.php
@@ -49,16 +49,21 @@ class easyFancyBox_Admin extends easyFancyBox {
 
 	// add our FancyBox Media Settings Section on Settings > Media admin page
 	public static function settings_section() {
-		echo '<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Easy%20FancyBox&item_number='.EASY_FANCYBOX_VERSION.'&no_shipping=0&tax=0&charset=UTF%2d8&currency_code=EUR" title="'.__('Donate to keep the Easy FancyBox plugin development going!','easy-fancybox').'"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" style="border:none;float:right;margin:5px 0 0 10px" alt="'.__('Donate to keep the Easy FancyBox plugin development going!','easy-fancybox').'" width="92" height="26" /></a>'.sprintf(__('The options in this section are provided by the plugin %s and determine the <strong>Media Lightbox</strong> overlay appearance and behavior controlled by %s.','easy-fancybox'),'<strong><a href="http://status301.net/wordpress-plugins/easy-fancybox/">'.__('Easy FancyBox','easy-fancybox').'</a></strong>','<strong><a href="http://fancybox.net/">'.__('FancyBox','easy-fancybox').'</a></strong>').'</p><p>'.__('First enable each sub-section that you need. Then save and come back to adjust its specific settings.','easy-fancybox').' '.__('Note: Each additional sub-section and features like <em>Auto-detection</em>, <em>Elastic transitions</em> and all <em>Easing effects</em> (except Swing) will have some extra impact on client-side page speed. Enable only those sub-sections and options that you actually need on your site.','easy-fancybox').' '.__('Some setting like Transition options are unavailable for SWF video, PDF and iFrame content to ensure browser compatibility and readability.','easy-fancybox').'</p>';
+		echo '<style type="text/css">.options-media-php br { display: initial; }</style><!-- undo WP style rule introduced in 4.9 on settings-media -->
+		<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Easy%20FancyBox&item_number='.EASY_FANCYBOX_VERSION.'&no_shipping=0&tax=0&charset=UTF%2d8&currency_code=EUR" title="'.__('Donate to keep the Easy FancyBox plugin development going!','easy-fancybox').'"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" style="border:none;float:right;margin:5px 0 0 10px" alt="'.__('Donate to keep the Easy FancyBox plugin development going!','easy-fancybox').'" width="92" height="26" /></a>'.sprintf(__('The options in this section are provided by the plugin %s and determine the <strong>Media Lightbox</strong> overlay appearance and behavior controlled by %s.','easy-fancybox'),'<strong><a href="http://status301.net/wordpress-plugins/easy-fancybox/">'.__('Easy FancyBox','easy-fancybox').'</a></strong>','<strong><a href="http://fancybox.net/">'.__('FancyBox','easy-fancybox').'</a></strong>').'</p><p>'.__('First enable each sub-section that you need. Then save and come back to adjust its specific settings.','easy-fancybox').' '.__('Note: Each additional sub-section and features like <em>Auto-detection</em>, <em>Elastic transitions</em> and all <em>Easing effects</em> (except Swing) will have some extra impact on client-side page speed. Enable only those sub-sections and options that you actually need on your site.','easy-fancybox').' '.__('Some setting like Transition options are unavailable for SWF video, PDF and iFrame content to ensure browser compatibility and readability.','easy-fancybox').'</p>';
 
 		/* Black Friday offer */
 		if ( !class_exists('easyFancyBox_Advanced')
 			&& current_user_can( 'install_plugins' )
 			&& strtotime('now') > strtotime('23-11-2017')
 			&& strtotime('now') < strtotime('28-11-2017') ) {
-			echo '<p style="background-color:#F9D400;padding:5px 10px;border-radius:15px;box-shadow:#333 0 1px 1px;color:#000;"><strong>Easy FancyBox advanced options at 30% OFF!</strong> Black Friday to Cyber Monday: THE BIG 30 SALE at Status301. <em>A whopping 30% discount but only for the first 30 customers.
+			echo '<p style="background-color:#F9D400;padding:5px 10px;border-radius:15px;box-shadow:#333 0 1px 1px;color:#000;">
+				<strong>Easy FancyBox advanced options at 30% OFF!</strong>
+				Black Friday to Cyber Monday: THE BIG 30 SALE at Status301.
+				<em>A whopping 30% discount but only for the first 30 customers.
 				After that, there will still be a discount of 15% for everybody until tuesday 0:00 GMT so
-				<strong><a href="https://premium.status301.net/black-friday-til-cyber-monday-big-30-sale/?discount=BFCM30" target="_blank">to take advantage of this opportunity</a></strong> before it\'s too late</em>...</p>';
+				<strong><a href="https://premium.status301.net/black-friday-til-cyber-monday-big-30-sale/?discount=BFCM30" target="_blank">to take advantage of this opportunity</a></strong>
+				before it\'s too late</em>...</p>';
 		}
 
 		// Pro extension version compatibility message
@@ -216,7 +221,7 @@ class easyFancyBox_Admin extends easyFancyBox {
 			echo '<div class="update-nag"><p>';
 			//echo '<a href="?easy_fancybox_ignore_notice=1" title="' . __('Hide message','easy-fancybox') . '" style="display:block;float:right">X</a>';
 			_e('Notice: The current Easy FancyBox plugin version is not fully compatible with your version of the Pro extension. Some advanced options may not be functional.','easy-fancybox');
-			echo '<br/>';
+			echo '<br />';
 			printf(__('Please <a href="%1$s" target="_blank">download and install the latest Pro version</a>.','easy-fancybox'), 'https://premium.status301.net/account/');
 			echo ' ';
 			printf(__('Or you can ignore and <a href="%1$s">hide this message</a>.','easy-fancybox'), '?easy_fancybox_ignore_notice=1');
@@ -253,12 +258,12 @@ class easyFancyBox_Admin extends easyFancyBox {
 		/* If user clicks to ignore the notice, add that to their user meta */
 		global $current_user;
 
-		if ( isset($_GET['easy_fancybox_ignore_notice']) && '1' == $_GET['easy_fancybox_ignore_notice'] ) {
+		if ( isset($_GET['easy_fancybox_ignore_notice']) && '1' == $_GET['easy_fancybox_ignore_notice'] )
 			add_user_meta($current_user->ID, 'easy_fancybox_ignore_notice', 'true', true);
-		}
 
 		if ( class_exists('easyFancyBox_Advanced')
-				&& ( !defined('easyFancyBox_Advanced::VERSION') || version_compare(easyFancyBox_Advanced::VERSION, self::$compat_pro_min, '<') ) )
+				&& ( !defined('easyFancyBox_Advanced::VERSION')
+				|| version_compare(easyFancyBox_Advanced::VERSION, self::$compat_pro_min, '<') ) )
 			self::$do_compat_warning = true;
 	}
 
