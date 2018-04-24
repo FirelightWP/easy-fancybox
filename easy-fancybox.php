39770405e3a849668f41ae92e2bae7e715e6e188
RavanH <ravanhagen@gmail.com>
2018-04-25 00:49:43 +0200

    update easing version number

--- easy-fancybox.php
+++ easy-fancybox.php
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 define( 'EASY_FANCYBOX_VERSION', '1.8' );
 define( 'FANCYBOX_VERSION', '1.3.14' );
 define( 'MOUSEWHEEL_VERSION', '3.1.13' );
-define( 'EASING_VERSION', '1.4.0' );
+define( 'EASING_VERSION', '1.4.1' );
 define( 'METADATA_VERSION', '2.22.1' );
 
 /**************
