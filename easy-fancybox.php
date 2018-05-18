4a283fe7af38627e3d7dcfb54044daea9aca7227
RavanH <ravanhagen@gmail.com>
2018-05-18 21:53:44 +0200

    don't prevent click when intercepted by minViewportWidth

--- easy-fancybox.php
+++ easy-fancybox.php
@@ -38,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  **************/
 
 define( 'EASY_FANCYBOX_VERSION', '1.8.3' );
-define( 'FANCYBOX_VERSION', '1.3.18' );
+define( 'FANCYBOX_VERSION', '1.3.19' );
 define( 'MOUSEWHEEL_VERSION', '3.1.13' );
 define( 'EASING_VERSION', '1.4.1' );
 define( 'METADATA_VERSION', '2.22.1' );
