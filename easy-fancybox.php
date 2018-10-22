e78c332f452553230a8cf00cf86013f282c70368
RavanH <ravanhagen@gmail.com>
2018-10-22 15:45:26 +0200

    version

--- easy-fancybox.php
+++ easy-fancybox.php
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * CONSTANTS
  **************/
 
-define( 'EASY_FANCYBOX_VERSION', '1.8.8' );
+define( 'EASY_FANCYBOX_VERSION', '1.8.9' );
 define( 'FANCYBOX_VERSION', '1.3.23' );
 define( 'MOUSEWHEEL_VERSION', '3.1.13' );
 define( 'EASING_VERSION', '1.4.1' );
