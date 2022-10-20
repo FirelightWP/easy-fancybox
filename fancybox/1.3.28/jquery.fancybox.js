0723f7c09022c1462bc1df7c545fe1049b9b2b7d
RavanH <ravanhagen@gmail.com>
2022-10-20 22:41:53 +0200

    version

--- fancybox/1.3.28/jquery.fancybox.js
+++ fancybox/1.3.28/jquery.fancybox.js
@@ -7,7 +7,7 @@
  * Copyright (c) 2008 - 2010 Janis Skarnelis
  * That said, it is hardly a one-person project. Many people have submitted bugs, code, and offered their advice freely. Their support is greatly appreciated.
  *
- * Version: 1.3.27 (2019/04/07)
+ * Version: 1.3.28 (2019/04/07)
  * Requires: jQuery v1.7+
  *
  * Dual licensed under the MIT and GPL licenses:
