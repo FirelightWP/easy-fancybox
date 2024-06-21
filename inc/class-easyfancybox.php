ba8e9958561de2358c3f0dd04447ce1d5811a0b6
Erick Danzer <edanzer@gmail.com>
2024-06-21 12:51:54 -0600

    Refactor: Coding standards for easyFancyBox class (#55)

--- inc/class-easyfancybox.php
+++ inc/class-easyfancybox.php
@@ -1,62 +1,240 @@
 <?php
 /**
- * Easy FancyBox Class
+ * This file defines the main easyFancyBox class for the plugin.
+ *
+ * @package   Easy_FancyBox
+ * @author    FirelightWP
+ * @license GPL-2.0+
+ *
+ * @version 1.0.0
  */
 
-class easyFancyBox {
+/**
+ * The main Easy FancyBox class.
+ *
+ * Handles general and frontend logic.
+ * The easyFancyBox_Admin handles admin logic.
+ *
+ * Class name should uppercase, but defined as lowercase long ago.
+ * We leave it as is for backward compatibility.
+ *
+ * @since 1.0.0
+ */
+class easyFancyBox { // phpcs:ignore
 
+	/**
+	 * URL for the plugin.
+	 *
+	 * @var string
+	 */
 	public static $plugin_url;
 
+	/**
+	 * Priority level for the plugin.
+	 *
+	 * @var int
+	 */
 	public static $priority;
 
+	/**
+	 * Base name for the plugin.
+	 *
+	 * @var string
+	 */
 	public static $plugin_basename;
 
+	/**
+	 * Flag to determine if scripts should be added.
+	 *
+	 * @var bool
+	 */
 	public static $add_scripts;
 
-	public static $styles         = array();
-	public static $inline_styles  = array();
-	public static $scripts        = array();
+	/**
+	 * Array to store styles.
+	 *
+	 * @var array
+	 */
+	public static $styles = array();
+
+	/**
+	 * Array to store inline styles.
+	 *
+	 * @var array
+	 */
+	public static $inline_styles = array();
+
+	/**
+	 * Array to store scripts.
+	 *
+	 * @var array
+	 */
+	public static $scripts = array();
+
+	/**
+	 * Array to store inline scripts.
+	 *
+	 * @var array
+	 */
 	public static $inline_scripts = array();
 
+	/**
+	 * URL for the style.
+	 *
+	 * @var string
+	 */
 	public static $style_url;
 
+	/**
+	 * URL for the IE-specific style.
+	 *
+	 * @var string
+	 */
 	public static $style_ie_url;
 
+	/**
+	 * URL for the script.
+	 *
+	 * @var string
+	 */
 	public static $script_url;
 
+	/**
+	 * Inline script.
+	 *
+	 * @var string
+	 */
 	public static $inline_script;
 
+	/**
+	 * Inline style.
+	 *
+	 * @var string
+	 */
 	public static $inline_style;
 
+	/**
+	 * IE-specific inline style.
+	 *
+	 * @var string
+	 */
 	public static $inline_style_ie;
 
+	/**
+	 * URL for the easing script.
+	 *
+	 * @var string
+	 */
 	public static $easing_script_url;
 
+	/**
+	 * URL for the mousewheel script.
+	 *
+	 * @var string
+	 */
 	public static $mousewheel_script_url;
 
+	/**
+	 * URL for the metadata script.
+	 *
+	 * @var string
+	 */
 	public static $metadata_script_url;
 
+	/**
+	 * Flag to determine if onready_auto is enabled.
+	 *
+	 * @var bool
+	 */
 	public static $onready_auto = false;
 
+	/**
+	 * Array to store options.
+	 *
+	 * @var array
+	 */
 	public static $options = array();
 
+	/**
+	 * Array to store events.
+	 *
+	 * @var array
+	 */
 	public static $events = array( 'post-load' );
 
-	public static $pro_plugin_url = "https://firelightwp.com/easy-fancybox-pro/";
+	/**
+	 * URL for the pro plugin.
+	 *
+	 * @var string
+	 */
+	public static $pro_plugin_url = 'https://firelightwp.com/pro-lightbox/';
 
+	/**
+	 * Array to store free lightboxes.
+	 *
+	 * @var array
+	 */
 	public static $free_lightboxes;
 
 	/**
-	 * Retrieves a list of all available lightboxes
+	 * Constructor for the class.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		global $wp_version;
+		self::$plugin_url      = plugins_url( '/', EASY_FANCYBOX_BASENAME /* EASY_FANCYBOX_DIR.'/easy-fancybox.php' */ );
+		self::$free_lightboxes = array(
+			'legacy'          => esc_html__( 'FancyBox Legacy', 'easy-fancybox' ),
+			'classic'         => esc_html__( 'FancyBox Classic Reloaded', 'easy-fancybox' ),
+			'fancyBox2'       => esc_html__( 'FancyBox V2', 'easy-fancybox' ),
+			'fancybox5-promo' => esc_html__( 'Pro Lightbox', 'easy-fancybox' ),
+		);
+
+		// Frontend hooks.
+		add_action( 'init', array( __CLASS__, 'extend' ), 9 );
+
+		// Disable core lightbox.
+		if (
+			isset( $wp_version )
+			&& version_compare( $wp_version, '6.5.0' ) >= 0
+			&& self::should_disable_core_lightbox()
+		) {
+			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'disable_core_lightbox_on_frontend' ), 99 );
+			add_filter( 'wp_theme_json_data_user', array( __CLASS__, 'hide_core_lightbox_in_editor' ) );
+		}
+	}
+
+	/**
+	 * Retrieve the lightboxes.
+	 *
+	 * Gets list of available lightboxes.
+	 * Also defines firelight_get_lightboxes filter that
+	 * can be used elsewhere to add lightboxes to the array.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @return array Returns an array of lightboxes.
 	 */
 	public static function get_lightboxes() {
 		return apply_filters( 'firelight_get_lightboxes', self::$free_lightboxes );
 	}
 
-	public static function enqueue_scripts()
-	{
+	/**
+	 * Enqueue scripts and styles.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public static function enqueue_scripts() {
 		// Make sure whe actually need to do anything.
-		if ( ! self::add_scripts() ){
+		if ( ! self::add_scripts() ) {
 			return;
 		}
 
@@ -65,15 +243,15 @@ class easyFancyBox {
 		$_ver    = defined( 'WP_DEBUG' ) && WP_DEBUG ? time() : false;
 		$_footer = get_option( 'fancybox_noFooter', false ) ? false : true;
 
-		// ENQUEUE STYLES
-		if ( !empty( self::$styles ) ) {
+		// Enqueue styles.
+		if ( ! empty( self::$styles ) ) {
 			foreach ( self::$styles as $handle => $options ) {
-				$src   = ! empty( $options['src'] )   ?         $options['src']   : '';
-				$deps  = ! empty( $options['deps'] )  ? (array) $options['deps']  : array();
-				$ver   = ! empty( $options['ver'] )   ?         $options['ver']   : $_ver;
-				$media = ! empty( $options['media'] ) ?         $options['media'] : 'all';
+				$src   = ! empty( $options['src'] ) ? $options['src'] : '';
+				$deps  = ! empty( $options['deps'] ) ? (array) $options['deps'] : array();
+				$ver   = ! empty( $options['ver'] ) ? $options['ver'] : $_ver;
+				$media = ! empty( $options['media'] ) ? $options['media'] : 'all';
 				wp_enqueue_style( $handle, $src, $deps, $ver, $media );
-				if ( ! empty( $options['conditional']) ) {
+				if ( ! empty( $options['conditional'] ) ) {
 					$wp_styles->add_data( $handle, 'conditional', $options['conditional'] );
 				}
 			}
@@ -85,12 +263,12 @@ class easyFancyBox {
 			}
 		}
 
-		// ENQUEUE SCRIPTS
-		if ( !empty( self::$scripts ) ) {
+		// Enqueue JS.
+		if ( ! empty( self::$scripts ) ) {
 			foreach ( self::$scripts as $handle => $options ) {
-				$src   = ! empty( $options['src'] )   ?         $options['src']   : '';
-				$deps  = ! empty( $options['deps'] )  ? (array) $options['deps']  : array();
-				$ver   = ! empty( $options['ver'] )   ?         $options['ver']   : $_ver;
+				$src  = ! empty( $options['src'] ) ? $options['src'] : '';
+				$deps = ! empty( $options['deps'] ) ? (array) $options['deps'] : array();
+				$ver  = ! empty( $options['ver'] ) ? $options['ver'] : $_ver;
 				wp_enqueue_script( $handle, $src, $deps, $ver, ! empty( $options['footer'] ) );
 			}
 		} else {
@@ -114,32 +292,52 @@ class easyFancyBox {
 		}
 
 		// Inline styles.
-		if ( !empty( self::$inline_styles ) ) {
+		if ( ! empty( self::$inline_styles ) ) {
 			foreach ( self::$inline_styles as $handle => $data ) {
 				if ( function_exists( 'wp_add_inline_style' ) && ! get_option( 'fancybox_pre45Compat', false ) ) {
 					wp_add_inline_style( $handle, $data );
 				} else {
 					// Do it the old way.
-					add_action( 'wp_head', function() use ( $data ) { print( '<style id="fancybox-inline-css" type="text/css">' . $data . '</style>' ); }, self::priority() );
+					add_action(
+						'wp_head',
+						function () use ( $data ) {
+							print( '<style id="fancybox-inline-css" type="text/css">' . $data . '</style>' );
+						},
+						self::priority()
+					);
 				}
 			}
-		} else {
+		} else { // phpcs:ignore
 			if ( function_exists( 'wp_add_inline_style' ) && ! get_option( 'fancybox_pre45Compat', false ) ) {
-				empty( self::$inline_style )    || wp_add_inline_style( 'fancybox', self::$inline_style );
+				empty( self::$inline_style ) || wp_add_inline_style( 'fancybox', self::$inline_style );
 				empty( self::$inline_style_ie ) || wp_add_inline_style( 'fancybox-ie', self::$inline_style_ie );
 			} else {
 				// Do it the old way.
-				empty( self::$inline_style )    || add_action( 'wp_head', function() { print( '<style id="fancybox-inline-css" type="text/css">' . self::$inline_style . '</style>' ); }, self::priority() );
-				empty( self::$inline_style_ie ) || add_action( 'wp_head', function() { print( '<!--[if lt IE 9]><style id="fancybox-inline-css-ie" type="text/css">' . self::$inline_style_ie . '</style><![endif]-->' ); }, self::priority() );
+				empty( self::$inline_style ) || add_action(
+					'wp_head',
+					function () {
+						print(
+							'<style id="fancybox-inline-css" type="text/css">' . self::$inline_style . '</style>'
+						);
+					},
+					self::priority()
+				);
+				empty( self::$inline_style_ie ) || add_action(
+					'wp_head',
+					function () {
+						print( '<!--[if lt IE 9]><style id="fancybox-inline-css-ie" type="text/css">' . self::$inline_style_ie . '</style><![endif]-->' );
+					},
+					self::priority()
+				);
 			}
 		}
 
 		// Inline scripts.
-		if ( !empty( self::$inline_scripts ) ) {
+		if ( ! empty( self::$inline_scripts ) ) {
 			foreach ( self::$inline_scripts as $handle => $data ) {
 				if ( is_array( $data ) ) {
 					$position = ! empty( $data['position'] ) ? $data['position'] : 'after';
-					$data = ! empty( $data['data'] ) ? $data['data'] : '';
+					$data     = ! empty( $data['data'] ) ? $data['data'] : '';
 				}
 				if ( function_exists( 'wp_add_inline_script' ) && ! get_option( 'fancybox_pre45Compat', false ) ) {
 					wp_add_inline_script( $handle, $data, $position );
@@ -147,34 +345,48 @@ class easyFancyBox {
 					// Do it the old way.
 					$priority = self::priority();
 					if ( 'after' !== $position ) {
-						$priority = $priority - 1;
+						$priority = $priority - 1; // phpcs:ignore
 					}
-					add_action( $_footer ? 'wp_footer' : 'wp_head', function() use ( $data ) { print( '<script type="text/javascript">' . $data . '</script>' ); }, $priority );
+					add_action(
+						$_footer ? 'wp_footer' : 'wp_head',
+						function () use ( $data ) {
+							print( '<script type="text/javascript">' . $data . '</script>' );
+						},
+						$priority
+					);
 				}
 			}
-		} else {
+		} else { // phpcs:ignore
 			if ( function_exists( 'wp_add_inline_script' ) && ! get_option( 'fancybox_pre45Compat', false ) ) {
-				empty( self::$inline_script )   || wp_add_inline_script( 'jquery-fancybox', self::$inline_script );
+				empty( self::$inline_script ) || wp_add_inline_script( 'jquery-fancybox', self::$inline_script );
 			} else {
 				// Do it the old way.
-				empty( self::$inline_script )   || add_action( $_footer ? 'wp_footer' : 'wp_head', function() { print( '<script type="text/javascript">' . self::$inline_script . '</script>' ); }, self::priority() );
+				empty( self::$inline_script ) || add_action(
+					$_footer ? 'wp_footer' : 'wp_head',
+					function () {
+						print( '<script type="text/javascript">' . self::$inline_script . '</script>' );
+					},
+					self::priority()
+				);
 			}
 		}
-
 	}
 
 	/**
-	 * Removes filter and dequeues JS for core lightbox.
-	 * Note: we may want to make this dependent on a setting.
-	 * Note: we may want to add this to free version of plugin.
+	 * Determines if the core lightbox should be disabled.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @return bool Returns true if the core lightbox should be disabled, false otherwise.
 	 */
 	public static function should_disable_core_lightbox() {
 		$selected_lightbox = get_option( 'fancybox_scriptVersion', 'classic' );
-		$is_free_lightbox = in_array( $selected_lightbox, array_keys( self::$free_lightboxes ) );
-		$is_pro_lightbox = 'fancybox5' === $selected_lightbox;
+		$is_free_lightbox  = in_array( $selected_lightbox, array_keys( self::$free_lightboxes ), true );
+		$is_pro_lightbox   = 'fancybox5' === $selected_lightbox;
 
 		$disable_for_free_lightboxes = get_option( 'fancybox_disableCoreLightbox', true );
-		$disable_for_pro_lightbox = get_option( 'fancybox5_disable_core_lightbox', true );
+		$disable_for_pro_lightbox    = get_option( 'fancybox5_disable_core_lightbox', true );
 
 		if ( $is_free_lightbox && $disable_for_free_lightboxes ) {
 			return true;
@@ -188,13 +400,17 @@ class easyFancyBox {
 	}
 
 	/**
-	 * Removes filter and dequeues JS for core lightbox.
-	 * Note: we may want to make this dependent on a setting.
-	 * Note: we may want to add this to free version of plugin.
+	 * Disables the core lightbox on the frontend.
+	 *
+	 * Details:
+	 * https://github.com/WordPress/gutenberg/blob/8dc36f6d30cc163671bdaa33f0656fdfe91f1447/packages/block-library/src/image/index.php#L64
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @return void
 	 */
 	public static function disable_core_lightbox_on_frontend() {
-		// These are added for the core lightbox here:
-		// https://github.com/WordPress/gutenberg/blob/8dc36f6d30cc163671bdaa33f0656fdfe91f1447/packages/block-library/src/image/index.php#L64
 		if ( function_exists( 'wp_dequeue_script_module' ) ) {
 			wp_dequeue_script_module( '@wordpress/block-library/image' );
 		}
@@ -204,7 +420,15 @@ class easyFancyBox {
 
 
 	/**
-	 * Disables/hides core lightbox for editor > image blocks
+	 * Hides the core lightbox in the editor.
+	 * Does this by filtering $theme_json.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @param obj $theme_json The theme JSON configuration.
+	 *
+	 * @return obj Filtered theme_json.
 	 */
 	public static function hide_core_lightbox_in_editor( $theme_json ) {
 		$new_data = array(
@@ -214,7 +438,7 @@ class easyFancyBox {
 					'core/image' => array(
 						'lightbox' => array(
 							'allowEditing' => false,
-							'enabled' => false,
+							'enabled'      => false,
 						),
 					),
 				),
@@ -223,31 +447,47 @@ class easyFancyBox {
 		return $theme_json->update_with( $new_data );
 	}
 
-	// Hack to fix missing wmode in Youtube oEmbed code based on David C's code in the comments on
-	// http://www.mehigh.biz/wordpress/adding-wmode-transparent-to-wordpress-3-media-embeds.html
-	// without the wmode, videos will float over the light box no matter what z-index is set.
-	public static function add_video_wmode_opaque( $html )
-	{
+	/**
+	 * Add video wmode opaque.
+	 *
+	 * Hack to fix missing wmode in Youtube oEmbed code based on David C's code in the comments on
+	 * http://www.mehigh.biz/wordpress/adding-wmode-transparent-to-wordpress-3-media-embeds.html
+	 * without the wmode, videos will float over the light box no matter what z-index is set.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param string $html Content to filter.
+	 * @return string $html Fitlered content.
+	 */
+	public static function add_video_wmode_opaque( $html ) {
 		// Make sure whe actually need this at all.
 		if ( ! self::add_scripts() ) {
 			return $html;
 		}
 
-		if ( strpos($html, "<embed src=" ) !== false ) {
-			$html = str_replace('</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque"', $html);
-		} elseif ( strpos($html, 'youtube' ) !== false && strpos($html, 'wmode' ) == false ) {
-			$html = str_replace('feature=oembed', 'feature=oembed&amp;wmode=opaque', $html);
-		} elseif ( strpos($html, "vimeo" ) !== false  && strpos($html, 'wmode' ) == false ) {
-			$html = str_replace('" width', '?theme=none&amp;wmode=opaque" width', $html);
-		} elseif ( strpos($html, "dailymotion" ) !== false  && strpos($html, 'wmode' ) == false ) {
-			$html = str_replace('" width', '?wmode=opaque" width', $html);
+		if ( strpos( $html, '<embed src=' ) !== false ) {
+			$html = str_replace( '</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque"', $html );
+		} elseif ( strpos( $html, 'youtube' ) !== false && strpos( $html, 'wmode' ) === false ) {
+			$html = str_replace( 'feature=oembed', 'feature=oembed&amp;wmode=opaque', $html );
+		} elseif ( strpos( $html, 'vimeo' ) !== false && strpos( $html, 'wmode' ) === false ) {
+			$html = str_replace( '" width', '?theme=none&amp;wmode=opaque" width', $html );
+		} elseif ( strpos( $html, 'dailymotion' ) !== false && strpos( $html, 'wmode' ) === false ) {
+			$html = str_replace( '" width', '?wmode=opaque" width', $html );
 		}
 
 		return $html;
 	}
 
-	public static function priority()
-	{
+	/**
+	 * Get or set the priority.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return int Returns the priority of the object.
+	 */
+	public static function priority() {
 		if ( null === self::$priority ) {
 			$priority = get_option( 'fancybox_scriptPriority' );
 
@@ -257,8 +497,15 @@ class easyFancyBox {
 		return self::$priority;
 	}
 
-	public static function add_scripts()
-	{
+	/**
+	 * Add scripts to the page.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return bool $add_scripts
+	 */
+	public static function add_scripts() {
 		if ( null === self::$add_scripts ) {
 			_doing_it_wrong( __FUNCTION__, 'Method easyFancyBox::add_scripts() has been called before init.', '2.0' );
 			return false;
@@ -267,15 +514,25 @@ class easyFancyBox {
 		return self::$add_scripts;
 	}
 
-	public static function extend()
-	{
+	/**
+	 * Applies fancybox lightbox based on which lightbox is selected.
+	 *
+	 * This only handles the original fancybox scripts for now.
+	 * If Pro lightbox is selected, switch here will not load anything.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public static function extend() {
 		$script_version = get_option( 'fancybox_scriptVersion', 'classic' );
 		if ( empty( self::$options ) ) {
 			include EASY_FANCYBOX_DIR . '/inc/fancybox-options.php';
 			self::$options = $efb_options;
 		}
 		foreach ( self::$options['Global']['options']['Enable']['options'] as $value ) {
-			if ( isset($value['id']) && '1' == get_option($value['id'],$value['default']) ) {
+			if ( isset( $value['id'] ) && '1' === get_option( $value['id'], $value['default'] ) ) {
 				self::$add_scripts = true;
 				break;
 			} else {
@@ -283,7 +540,7 @@ class easyFancyBox {
 			}
 		}
 
-		switch( $script_version ) {
+		switch ( $script_version ) {
 			case 'legacy':
 				include EASY_FANCYBOX_DIR . '/inc/fancybox-legacy.php';
 				break;
@@ -302,35 +559,5 @@ class easyFancyBox {
 		}
 
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ), self::priority() );
-	}
-
-	/**
-	 * RUN
-	 */
-
-	public function __construct()
-	{
-		global $wp_version;
-
-		// VARS
-		self::$plugin_url = plugins_url( '/', EASY_FANCYBOX_BASENAME /* EASY_FANCYBOX_DIR.'/easy-fancybox.php' */ );
-		self::$free_lightboxes = array(
-			'legacy' => esc_html__( 'FancyBox Legacy', 'easy-fancybox' ),
-			'classic' => esc_html__( 'FancyBox Classic Reloaded', 'easy-fancybox' ),
-			'fancyBox2' => esc_html__( 'FancyBox V2', 'easy-fancybox' ),
-			'fancybox5-promo' => esc_html__( 'Pro Lightbox', 'easy-fancybox' ),
-		);
-
-		add_action( 'init', array( __CLASS__, 'extend' ), 9 );
-
-		// Disable core lightbox
-		if (
-			isset( $wp_version )
-			&& version_compare( $wp_version, '6.5.0' ) >= 0
-			&& self::should_disable_core_lightbox()
-		) {
-			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'disable_core_lightbox_on_frontend' ), 99 );
-			add_filter( 'wp_theme_json_data_user', array( __CLASS__, 'hide_core_lightbox_in_editor' ) );
-		}
 	}
 }
