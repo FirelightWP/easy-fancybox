4d83aa0a5269c8ca039621e807551ec65aef3c33
edanzer <edanzer@gmail.com>
2024-06-27 14:51:46 -0600

    Rerun build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '69b294e2a2581f582f92');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '06de885016b657a2570f');
