efcff28c6434a17fd732aaa220194ba8e5e9d868
edanzer <edanzer@gmail.com>
2024-07-11 13:38:31 -0600

    Fix NextGEN Gallery grouping

--- inc/fancybox-legacy.php
+++ inc/fancybox-legacy.php
@@ -103,7 +103,10 @@ var fb_'.$key.'_select=jQuery(\'';
 				}
 				$script .= '\');';
 
-				$autoselector = class_exists('easyFancyBox_Advanced') ? \get_option($value['options']['autoSelector']['id'],$value['options']['autoSelector']['default']) : $value['options']['autoSelector']['default'];
+				$nextgen_selectors = '.ngg-galleryoverview, .ngg-imagebrowser, .nextgen_pro_blog_gallery, .nextgen_pro_film, .nextgen_pro_horizontal_filmstrip, .ngg-pro-masonry-wrapper, .ngg-pro-mosaic-container, .nextgen_pro_sidescroll, .nextgen_pro_slideshow, .nextgen_pro_thumbnail_grid, .tiled-gallery';
+				$autoselector      = class_exists( 'easyFancyBox_Advanced' )
+					? \get_option( $value['options']['autoSelector']['id'], $value['options']['autoSelector']['default'] ) . ',' . $nextgen_selectors
+					: $value['options']['autoSelector']['default'];
 
 				// Class and rel depending on settings.
 				if( '1' === $autoAttributeLimit || 'sections' === $autoAttributeLimit ) {
