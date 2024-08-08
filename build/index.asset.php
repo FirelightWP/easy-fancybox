e032e0020c3a0319d3981e139f77f9b98ee02ddf
edanzer <edanzer@gmail.com>
2024-08-08 13:51:24 -0600

    Add link utm params

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '06de885016b657a2570f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '613c856ffb11a04fe2ac');
