8401a111d751946268d349f8be1dd7490e3b7da0
edanzer <edanzer@gmail.com>
2024-08-24 10:33:47 -0600

    Fix no scroll after lightbox close

--- fancybox/1.3.28/jquery.fancybox.js
+++ fancybox/1.3.28/jquery.fancybox.js
@@ -1336,11 +1336,11 @@
 		enableEscapeButton : true,
 		enableKeyboardNav : true,
 
-		onStart : function(){$("html, body").css("overflow","hidden");},
+		onStart : function(){},
 		onCancel : function(){},
 		onComplete : function(){},
 		onCleanup : function(){},
-		onClosed : function(){$("html, body").css("overflow","auto");},
+		onClosed : function(){},
 		onError : function(){}
 	};
 
