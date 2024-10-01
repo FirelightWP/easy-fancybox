829ef1bfd3d76fdc37feb280c777050b2f7a9217
edanzer <edanzer@gmail.com>
2024-09-30 22:55:58 -0600

    Update to version 2.3.4

--- easy-fancybox.php
+++ easy-fancybox.php
@@ -5,7 +5,7 @@
  * Description: Formerly Easy Fancybox. The most popular WordPress lightbox plugin. Simple, fast, and responsive. Opens images, videos, PDFs, and custom popups.
  * Text Domain: easy-fancybox
  * Domain Path: languages
- * Version: 2.3.3
+ * Version: 2.3.4
  * Author: FirelightWP
  * Author URI: https://firelightwp.com
  *
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * CONSTANTS
  */
-define( 'EASY_FANCYBOX_VERSION', '2.3.3' );
+define( 'EASY_FANCYBOX_VERSION', '2.3.4' );
 define(
 	'FANCYBOX_VERSIONS',
 	array(
