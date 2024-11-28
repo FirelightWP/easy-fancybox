db5d01183c1f152494dca96f1ddce4fdc06894bf
edanzer <edanzer@gmail.com>
2024-11-27 17:10:24 -0700

    Add black friday

--- inc/class-easyfancybox-admin.php
+++ inc/class-easyfancybox-admin.php
@@ -202,9 +202,9 @@ class easyFancyBox_Admin { // phpcs:ignore
 
 		if ( ! self::has_valid_license() && ! self::should_show_review_request() ) {
 			echo '<div class="sale-banner"><p>';
-			esc_html_e( 'DOUBLE PROMOTIONS! Try Firelight Pro free + lock in 20% off with PRO20 coupon!', 'easy-fancybox' );
+			esc_html_e( 'BLACK FRIDAY SALE! 40% OFF Firelight Pro. Use coupon BF2024 at checkout!', 'easy-fancybox' );
 			echo ' <a href="https://firelightwp.com/pro-lightbox?utm_source=pro-settings&utm_medium=referral&utm_campaign=easy-fancybox" target="_blank" class="banner-button">' . esc_html__( 'Demos', 'easy-fancybox' ) . '</a>';
-			echo ' <a href="https://firelightwp.com/pro-lightbox/pricing?utm_source=pro-settings&utm_medium=referral&utm_campaign=easy-fancybox" target="_blank" class="banner-button">' . esc_html__( 'Try Pro Free', 'easy-fancybox' ) . '</a>';
+			echo ' <a href="https://firelightwp.com/pro-lightbox/pricing?utm_source=pro-settings&utm_medium=referral&utm_campaign=easy-fancybox" target="_blank" class="banner-button">' . esc_html__( 'See Pricing', 'easy-fancybox' ) . '</a>';
 			echo '</p></div>';
 		}
 
