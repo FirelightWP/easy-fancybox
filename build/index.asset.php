1dc6e856ce2d601bec5994b082337fd5c0767181
Erick Danzer <edanzer@gmail.com>
2024-12-05 21:10:08 -0700

    Pro: Remove Cyber Monday (#144)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '84e3e5936c0ec9b6bc99');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => 'c8b40ae4cc3d09804140');
